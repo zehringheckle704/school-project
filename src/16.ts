8464143cfa34e0f438e5f8f2eed91aae2c8285e7
zehringheckle704 <zehringheckle704@gmail.com>
2025-03-22 15:03:58 +0000

    Add 16.ts

---
+++ src/16.ts
@@ -0,0 +1,13 @@
+function calculateGrade(score: number): string {
+    if (score >= 90) {
+        return "A";
+    } else if (score >= 80) {
+        return "B";
+    } else if (score >= 70) {
+        return "C";
+    } else if (score >= 60) {
+        return "D";
+    } else {
+        return "F";
+    }
+}
